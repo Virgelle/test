9074e4a357484e565b4d07957467ffda5efe81db
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-01-24 14:18:53 +0500

    Добавил пример шаблона

--- _init_.py
+++ _init_.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello  World!'
+    env = { 'page_title': 'Main Page' }
+    return render_template('layout.html', env=env)
 
 if __name__ == '__main__':
     app.run()
